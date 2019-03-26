aca37949496af62d0e7ca0bdaa74da6e5229dddb
macbook <macbook@10.71.255.80>
2019-03-26 20:15:33 -0300

    Aula 01

---
+++ Aula01/src/main/java/br/usjt/Aula01/repository/TempoRepository.java
@@ -0,0 +1,10 @@
+package br.usjt.Aula01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.usjt.Aula01.model.Tempo;
+
+public interface TempoRepository extends JpaRepository<Tempo, Long>{
+
+
+}
